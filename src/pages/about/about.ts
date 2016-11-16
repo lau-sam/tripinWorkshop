07703f7bf2a7c357a23e79c8272b1b8a47b1fdbe
samlau <onsixe@gmail.com>
2016-11-16 17:05:44 +0100

    GET Json data. Render data in screen

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+//import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
-  constructor(public navCtrl: NavController) {
-
+  m_json: any;
+ 	
+  constructor(public http: Http) {
+	/**this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=10').map(res => res.json()).subscribe(data => {
+	    this.m_json = data.data.children;
+	    console.log(this.m_json);
+	});*/
+	this.http.get('https://restcountries.eu/rest/v1/all').map(res => res.json()).subscribe(data => {
+	    this.m_json = data;
+	    //console.log(this.m_json);
+	});
   }
 
 }
