00b0e33c96eec5d6c022a27411c9c760afba4b95
davyazoulay <davy.azoulay@epsi.fr>
2016-11-17 10:09:04 +0100

    update home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { TabsPage } from '../tabs/tabs';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	tabsPage = TabsPage;
 
   constructor(public navCtrl: NavController) {
 
