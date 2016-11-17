07270dea1d97d1f3e13e87aafdd3b4e81e726b74
davyazoulay <davy.azoulay@epsi.fr>
2016-11-17 10:32:13 +0100

    page connexion n'est plus dans les tabs, page find Ã  la place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 import { HomePage } from '../pages/home/home';
 
 
+
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
-import { HomePage } from '../home/home';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
+import { FindPage } from '../find/find';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,7 +10,7 @@ import { ContactPage } from '../contact/contact';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  tab1Root: any = HomePage;
+  tab1Root: any = FindPage;
   tab2Root: any = AboutPage;
   tab3Root: any = ContactPage;
 
